4f97a44482d3ef369716bf5d33c03afab77d9116
Ufuk Kiper <info@pamecarbon.com>
2026-07-09 19:38:26 +0300

    kod geri getirildi

--- app.py
+++ app.py
@@ -576,6 +576,4 @@ def index():
     return render_page(content)
 
 if __name__ == "__main__":
-    # Render'ın verdiği PORT'u kullan, yoksa 10000 kullan
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.getenv("PORT", 5001))
